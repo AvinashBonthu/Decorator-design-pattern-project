c4ef2804044364b0950652c5728d3ce8dadf780d
Avinash <47055975+AvinashBonthu@users.noreply.github.com>
2021-02-17 11:04:28 +0530

    Add files via upload

---
+++ VegFood.java
@@ -0,0 +1,20 @@
+
+public class VegFood implements Food {
+	
+	
+	public String prepareFood()
+	{
+		
+		 return "Veg Food";
+		
+		
+	}
+
+	public double foodPrice()
+	{
+		System.out.println("Paneer curry");
+		return 50.0;
+		
+	}
+	
+}
